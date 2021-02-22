2fa8e94fda4cfe99af411766397e38902993ee71
Steven Carlson <stevencarlson@Stevens-MacBook-Air.local>
2021-02-21 19:15:05 -0600

    adding default mac setup

---
+++ mac/install.sh
@@ -0,0 +1,5 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> /Users/{profile}/.zprofile
+
+brew install git
+brew install ccleaner
