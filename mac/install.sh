200354435fdf322cb2577b81ef5b4530c71cc33d
Steven Carlson <steven.carlson007@gmail.com>
2021-10-17 23:13:55 -0500

    Updated installed list

--- mac/install.sh
+++ mac/install.sh
@@ -3,3 +3,9 @@ echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> /Users/{profile}/.zprofile
 
 brew install git
 brew install ccleaner
+brew install dos2unix
+brew install terraform-docs
+brew install docker
+brew install snyk
+brew install python@3.9
+brew install node-build
